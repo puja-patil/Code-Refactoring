257b5f32febecb853f0bb83c6a045e632f870b56
puja-patil <patilpuja1357@gmail.com>
2020-11-30 21:01:19 +0530

    refactoring #1

--- Refactoring Principles/PrintBill.js
+++ Refactoring Principles/PrintBill.js
@@ -10,25 +10,8 @@ function statement(invoice, plays) {
     }).format;
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
+        let thisAmount = amountFor(perf, play);
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`Unknown play type : ${play.type}`)
-        }
         //add volume crredits
         volumeCredits += Math.max(perf.audience - 30, 0);
         //add extra credit for every 10 comedy attendees
@@ -45,4 +28,25 @@ function statement(invoice, plays) {
     return result;
 }
 
+function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`Unknown play type : ${play.type}`)
+    }
+    return thisAmount;
+}
 console.log(statement(invoice, plays));
