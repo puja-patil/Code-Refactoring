32008c9f6cab278b593199e16e914597c37d4258
puja-patil <patilpuja1357@gmail.com>
2020-12-03 19:57:04 +0530

    refactoring - mapped volume credits to performances #21

--- Refactoring Principles/PrintBill.js
+++ Refactoring Principles/PrintBill.js
@@ -11,6 +11,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -39,6 +40,15 @@ function statement(invoice, plays) {
         }
         return result;
     }
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        //add volume crredits
+        result += Math.max(aPerformance.audience - 30, 0);
+        //add extra credit for every 10 comedy attendees
+        if ("comedy" === aPerformance.play.type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -63,7 +73,7 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -74,15 +84,6 @@ function renderPlainText(data, plays) {
         }).format(aNumber / 100);
     }
 
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        //add volume crredits
-        result += Math.max(aPerformance.audience - 30, 0);
-        //add extra credit for every 10 comedy attendees
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
 
 
 
