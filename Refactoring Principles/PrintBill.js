32a6e3d956f6639ba2c8d94611042ad613b29434
puja-patil <patilpuja1357@gmail.com>
2020-12-03 17:47:50 +0530

    refactoring - extracted fn for plain text rendering in HTML  #16

--- Refactoring Principles/PrintBill.js
+++ Refactoring Principles/PrintBill.js
@@ -2,75 +2,77 @@ const invoice = require('./invoices.json');
 const plays = require('./plays.json');
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
 
+}
 
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
-        //print line for this order
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience} seats\n`;
     }
-
     result += `Amount owed is ${usd(getTotalAmount())}\n`;
     result += `You earned  ${totalVolumeCredits()} Credits\n`;
     return result;
-}
 
-function getTotalAmount() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += amountFor(perf);
+
+
+    function getTotalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
-    return result;
-}
 
-function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += volumeCreditsFor(perf);
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
     }
-    return result;
-}
 
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format(aNumber / 100);
-}
-
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    //add volume crredits
-    result += Math.max(aPerformance.audience - 30, 0);
-    //add extra credit for every 10 comedy attendees
-    if ("comedy" === playFor(aPerformance).type)
-        result += Math.floor(aPerformance.audience / 5);
-    return result;
-}
-
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`Unknown play type : ${playFor(aPerformance).type}`)
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency", currency: "USD", minimumFractionDigits: 2
+        }).format(aNumber / 100);
     }
-    return result;
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        //add volume crredits
+        result += Math.max(aPerformance.audience - 30, 0);
+        //add extra credit for every 10 comedy attendees
+        if ("comedy" === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`Unknown play type : ${playFor(aPerformance).type}`)
+        }
+        return result;
+    }
 }
 console.log(statement(invoice, plays));
