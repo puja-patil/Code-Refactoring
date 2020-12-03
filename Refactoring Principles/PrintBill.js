07393ceeca5b56ad17761650b28e5917650e9a3a
puja-patil <patilpuja1357@gmail.com>
2020-12-03 17:26:35 +0530

    refactoring - extracted fn for totalVolumeCredits  #13

--- Refactoring Principles/PrintBill.js
+++ Refactoring Principles/PrintBill.js
@@ -3,21 +3,30 @@ const plays = require('./plays.json');
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
+
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
 
-        volumeCredits += volumeCreditsFor(perf);
+
 
         //print line for this order
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience} seats\n`;
         totalAmount += amountFor(perf);
 
     }
+    let volumeCredits = totalVolumeCredits();
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned Volume Credits : ${volumeCredits} volume Credits\n`;
+    result += `You earned  ${volumeCredits} Credits\n`;
     return result;
+}
+
+function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
 }
 
 function usd(aNumber) {
