7ccbe60c273ff54a3f6777af56d75fac66415574
puja-patil <patilpuja1357@gmail.com>
2020-12-04 11:09:08 +0530

    refactoring - removed amountFor() and volumeCredits()   #29.1

--- Refactoring Principles/statement.js
+++ Refactoring Principles/statement.js
@@ -105,13 +105,6 @@ function createStatementData(invoice, plays) {
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-
-    function amountFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
-    function volumeCreditsFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
-    }
     function getTotalAmount(data) {
         return data.performances.reduce((total, p) => total + p.amount, 0);
     }
